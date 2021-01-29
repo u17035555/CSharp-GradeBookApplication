183842e76e7d7d2e10b7d13dfe4342390c0d3879
Heinrich Carstens <Heinrich.Carstens@psg.co.za>
2021-01-29 12:44:48 +0200

    Overloaded RankedGradeBook GetLetterGrade method.

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -9,6 +8,52 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = Enums.GradeBookType.Ranked;
+        }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException();
+            }
+
+            double percentile = CalculatePercentile(averageGrade);
+
+            if (percentile <= 20)
+            {
+                return 'A';
+            }
+            else if (percentile <= 40)
+            {
+                return 'B';
+            }
+            else if (percentile <= 60)
+            {
+                return 'C';
+            }
+            else if (percentile <= 80)
+            {
+                return 'D';
+            }
+
+            return 'F';
+        }
+
+        public int GetRanking(double averageGrade)
+        {
+            return Students.Count(student => student.AverageGrade >= averageGrade);
+        }
+
+        public double CalculatePercentile(double averageGrade)
+        {
+            try
+            {
+                return (GetRanking(averageGrade) / (double) Students.Count) * 100;
+            }
+            catch (DivideByZeroException)
+            {
+                throw new InvalidOperationException();
+            }
         }
     }
 }
