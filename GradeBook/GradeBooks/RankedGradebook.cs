1082aa1cf821d4ebcc1f8a1f26cd376277608b49
Heinrich Carstens <Heinrich.Carstens@psg.co.za>
2021-01-29 11:40:07 +0200

    Added GradeBookType enum type.

---
+++ GradeBook/GradeBooks/RankedGradebook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class RankedGradebook : BaseGradeBook
+    {
+        public RankedGradebook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
---
+++ GradeBook/GradeBooks/StandardGradebook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    class StandardGradebook : BaseGradeBook
+    {
+        public StandardGradebook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Standard;
+        }
+    }
+}
